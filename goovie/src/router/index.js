33470401b9edeaceb72d44dab87d6899a96f6ea1
mleonor5 <mleonorspr@gmail.com>
2023-05-09 19:19:18 +0100

    adição alt + remoção do path about

--- goovie/src/router/index.js
+++ goovie/src/router/index.js
@@ -125,14 +125,6 @@ const routes = [
     path: '/:directorId',
     name: 'director',
     component: director
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/About.vue')
   }
 ]
 
